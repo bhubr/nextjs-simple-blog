a6c4f2d1964578c75e8b2d17807fc02eb3108ad5
Benoît Hubert <benoithubert@gmail.com>
2021-01-13 09:58:53 +0100

    Fetch from API in post page

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,15 +1,8 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import DefaultErrorPage from 'next/error'
-import db from '../static/db.json';
 
-function BlogPostDetailsContainer() {
-  // on ferait un appel API vers le backend pour récupérer un article via son slug
-  const router = useRouter()
-  const { slug } = router.query
-  // récupérer le bon post dans le tableau
-  const post = db.posts.find((p) => p.slug === slug);
-  console.log(slug, post)
+function BlogPostDetailsContainer({ post }) {
+  console.log(post)
   if(!post) {
     return (
       <>
@@ -26,6 +19,39 @@ function BlogPostDetailsContainer() {
       <div>{post.content}</div>
     </article>
   );
+}
+
+async function fetchAllPosts() {
+  const res = await fetch('https://my-json-server.typicode.com/bhubr/fake-blog-api/posts')
+  return res.json()
+}
+
+async function fetchOnePost(slug) {
+  const res = await fetch(`https://my-json-server.typicode.com/bhubr/fake-blog-api/posts/${slug}`)
+  return res.json()
+}
+
+export async function getStaticPaths() {
+  const posts = await fetchAllPosts()
+  const paths = posts.map(p => `/${p.id}`)
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+// // This function gets called at build time
+export async function getStaticProps({ params }) {
+  const { slug } = params
+  const post = await fetchOnePost(slug)
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      post,
+    },
+  }
 }
 
 export default BlogPostDetailsContainer;
